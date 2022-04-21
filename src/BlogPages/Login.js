d9c37ee4e21ec94f63707e9648486fe2092b8352
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-04-21 16:50:18 +0100

    added files

--- src/BlogPages/Login.js
+++ src/BlogPages/Login.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React, {useState} from "react";
 import {auth, provider} from "../firebase-config";
 import {signInWithPopup} from "firebase/auth";
 import {useNavigate} from "react-router-dom"
@@ -8,7 +8,21 @@ import {useNavigate} from "react-router-dom"
 
 
 
+const BudgetBody=(props)=>{
+    const {balance, setBalance}=props
+    const [budget, setBudget]=useState([])
+    let {identity}=useParams()
 
+    return (
+        <div className="budget-body">
+            <div className="text-intro">
+                <h1> Welcome to your Pesonal Budget{identity}</h1>
+                <p>Enter the price, description and give a special name</p>
+            </div>
+
+        </div>
+    )
+}
 
 function Login({setIsAuth}){
     let  navigate=useNavigate()
@@ -33,13 +47,26 @@ function Login({setIsAuth}){
 
 const handleLogin=()=> {
     let userRepository = JSON.parse(LocalStorage.getItem("users"))
+    let  user;
     let isValid = userRepository.some(userObject => {
             let idCheck = (userObject?.email === userDetails.identity ||
                 userObject?.username === userDetails?.identity)
-            let passwordCheck = (userObject.password === userDetails.password)
+            let passwordCheck = userObject.password === userDetails.password
+            if (
+                idCheck && passwordCheck
+            ){user=userObject}
             return idCheck && passwordCheck
         }
     )
+
+    let {identity}=userDetails
+    isValid ?
+        history.push('/dashboard/${identity}/?id=${user.username})/?id=${identity}'):
+        setAlert({ishow:true, status:"error", message:"user details incorrect"})
+    setTimeout(
+        ()=>setAlert({ishow:false, status:"", message:""})
+    )
+
 }
 
 export  default Login
